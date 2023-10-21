f15c85a43ac2f9228dd27b1624996edb0ef806e8
Tsybulskyi Evhenyi <etsy2900@gmail.com>
2023-10-21 12:28:11 +0300

    small speed test, clean code

--- fast_html/__init__.py
+++ fast_html/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 import re
 from typing import Iterator, Union, Optional
@@ -18,7 +18,7 @@ def indent_it(value):
 
 
 def render(gen: Tag) -> str:
-    return ''.join(render(t) for t in gen) if type(gen) == list else ''.join(gen)
+    return ''.join(render(t) for t in gen) if isinstance(gen, list) else ''.join(gen)
 
 
 def solo_tag(tag_name: str, **kwargs) -> Tag:
@@ -34,16 +34,16 @@ def solo_tag(tag_name: str, **kwargs) -> Tag:
     """
 
     kwargs = {
-        re.sub('_$', '', k).replace('_', '-'): v for k, v in kwargs.items() if v is not None and (type(v) != bool or v)
+        re.sub('_$', '', k).replace('_', '-'): v for k, v in kwargs.items() if v is not None and (not isinstance(v, bool) or v)
     }
 
-    attrs = ''.join(f' {k}' if type(v) == bool else f' {k}="{v}"' for k, v in kwargs.items())
+    attrs = ''.join(f' {k}' if isinstance(v, bool) else f' {k}="{v}"' for k, v in kwargs.items())
     yield f"<{tag_name}{attrs}>{_cr if indent else ''}"
 
 
 def _inner(inner: Inner):
     """unfold the inner iterators"""
-    if type(inner) == str:  # inner is a str
+    if isinstance(inner, str):  # inner is a str
         yield f'{_tab}{inner}' if indent else inner
     else:
         for i in inner:
@@ -69,11 +69,11 @@ def tag(tag_name: str, inner: Optional[Inner] = None, **kwargs) -> Tag:
     yield from solo_tag(tag_name, **kwargs)
 
     if inner is not None:
-        if type(inner) == str:  # inner is a str
+        if isinstance(inner, str):  # inner is a str
             yield f'{_tab}{inner}{_cr}' if indent else inner
         else:
             for i in inner:
-                if type(i) == str:  # inner is a Tag
+                if isinstance(i, str):  # inner is a Tag
                     yield f'{_tab}{i}' if indent else i
                 else:
                     for i1 in i:
--- test.py
+++ test.py
@@ -1,4 +1,20 @@
-import doctest
+import unittest
+import time
 
-if __name__ == '__main__':
-    doctest.testfile('README.rst')
+from fast_html import div
+
+
+class PerformanceTesting(unittest.TestCase):
+    def test_large_structure_generation_performance(self):
+        num_divs = 100000
+        start_time = time.time()
+        for _ in range(num_divs):
+            "".join(div("Content"))
+        end_time = time.time()
+        execution_time = end_time - start_time
+        max_execution_time = 0.130
+        self.assertLessEqual(execution_time, max_execution_time)
+
+
+if __name__ == "__main__":
+    unittest.main()
